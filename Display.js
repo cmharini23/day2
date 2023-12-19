71c2829c9eeefe210066949fbd61ad06f83701c7
cmharini23 <118423449+cmharini23@users.noreply.github.com>
2023-12-19 11:37:16 +0530

    Add files via upload

---
+++ Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+
+class Display extends Component{
+    render(){
+        return(
+            <div>
+                <h1>This is class component</h1>
+            </div>
+        );
+    }
+}
+export default Display;
---
+++ DisplayMsg.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function DisplayMsg(){
+    return(
+    <div>
+
+        <h1>Hello! Welcome to Functional component</h1>
+    </div>
+    );
+}
+export default DsisplayMsg;
